edd8f4987380c7a467b4a07328c401f1698d648d
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2016-04-28 06:19:19 -0400

    properly call fail

--- functions/transmogrify/handler.js
+++ functions/transmogrify/handler.js
@@ -9,7 +9,7 @@ module.exports.handler = function(event, context) {
   lib.process(event, function callback(results) {
     results.log()
     if(results.errors.length > 0)
-      context.fail('Error Occured')
+      context.fail()
     else
       context.done(null, results)
   })
